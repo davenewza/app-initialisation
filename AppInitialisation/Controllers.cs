9d5e0e05dba38cf3627a15fb1bf00f8d1a58acb2
Dave <sanodn@gmail.com>
2017-07-14 07:51:52 +0200

    Adding solution to source control

---
+++ AppInitialisation/Controllers.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AppInitialisation
+{
+    public class InitialisationModule
+    {
+        public bool IsWarm { get; private set; } = false;
+
+        public bool IsWarmingUp { get; private set; } = false;
+
+        public readonly TimeSpan Duration = TimeSpan.FromSeconds(500);
+        private ILogger _logger;
+
+        public InitialisationModule(ILogger<InitialisationModule> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Initialise()
+        {
+            if (IsWarm)
+            {
+                _logger.LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} already warm.");
+            }
+            else
+            {
+                IsWarmingUp = true;
+                _logger.LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} warming up.");
+                Task.Delay(Duration).Wait();
+                IsWarm = true;
+                IsWarmingUp = false;
+            }
+        }
+    }
+
+    [Route("api/things")]
+    public class Controllers : Controller
+    {
+        private InitialisationModule _initModule;
+        private ILogger _logger;
+
+        public Controllers(InitialisationModule initModule, ILogger<Controllers> logger)
+        {
+            _initModule = initModule;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} GET api/things.");
+
+            int count = 0;
+
+            if (!_initModule.IsWarm && !_initModule.IsWarmingUp)
+            {
+                _initModule.Initialise();
+            }
+
+            while (!_initModule.IsWarm)
+            {
+                _logger.LogInformation($"Not warm yet ({count++})");
+                Task.Delay(TimeSpan.FromSeconds(4)).Wait();
+            }
+
+            return Json(new { Message = "Cozy and warm!" });
+
+            //if (_initModule.IsWarm)
+            //{
+            //    return Json(new { Message = "Cozy and warm!" });
+            //}
+            //else
+            //{
+            //    return Json(new { Message = "I am not warmed up!" });
+            //}
+        }
+
+        [HttpGet("initialise")]
+        public IActionResult Initialise()
+        {
+            _logger.LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} initialising.");
+
+            _initModule.Initialise();
+
+            _logger.LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} initialised.");
+
+            return Json(new { Message = $"Ready." });
+        }
+    }
+}
---
+++ AppInitialisation/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace AppInitialisation
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                options.SerializerSettings.Culture = CultureInfo.InvariantCulture;
+            });
+
+            services.AddSingleton<InitialisationModule>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory
+                .AddConsole()
+                .AddAzureWebAppDiagnostics();
+
+            loggerFactory.CreateLogger<Startup>().LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} starting.");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Use MVC.
+            app.UseMvc();
+
+            loggerFactory.CreateLogger<Startup>().LogInformation($"{Environment.MachineName} {Process.GetCurrentProcess().Id} started.");
+        }
+    }
+}
